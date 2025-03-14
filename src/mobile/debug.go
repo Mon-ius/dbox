105b4403b5c01617f9db75bb6aa4930236ee77b6
m0nius <m0niusplus@gmail.com>
2025-03-15 01:37:14 +0800

    add android build ci

--- src/mobile/debug.go
+++ src/mobile/debug.go
@@ -3,9 +3,14 @@
 package mobile
 
 import (
+    "unsafe",
     "github.com/mon-ius/dbox/src/libdbox"
 )
 
 func PrintDebug(message string) {
     libdbox.PrintDebugMessage(message)
+}
+
+func FreeString(ptr unsafe.Pointer) {
+    libdbox.FreeStringPtr(ptr)
 }
